6b21096ab83bb2feee4fb436e5a7a64923c94237
Harry Scells <harryscells@gmail.com>
2018-03-15 13:20:36 +1000

    Add commands before archive.

--- cmd/windex/main.go
+++ cmd/windex/main.go
@@ -3,7 +3,15 @@ package main
 import (
 	"github.com/alexflint/go-arg"
 	"github.com/hscells/weasel"
-	"reflect"
+	"gopkg.in/olivere/elastic.v5"
+	"io"
+	"encoding/json"
+	"log"
+	"context"
+	"time"
+	"fmt"
+	"os"
+	"runtime/pprof"
 )
 
 type args struct {
@@ -20,13 +28,106 @@ func (args) Description() string {
 }
 
 func main() {
+	f, err := os.Create("weasel.prof")
+	if err != nil {
+		log.Fatal(err)
+	}
+	pprof.StartCPUProfile(f)
+	defer pprof.StopCPUProfile()
 
 	var args args
 	arg.MustParse(&args)
+	wi := weasel.NewInvertedIndex(args.Name,
+		map[string]weasel.Indexable{
+			"text":         weasel.IndexableString,
+			"text.stemmed": weasel.IndexableString,
+			"title":        weasel.IndexableString,
+		},
+		weasel.NoSource("text.stemmed"),
+		weasel.AddAnalyser("title", weasel.LowercaseFilter, weasel.StopEn),
+		weasel.AddAnalyser("text", weasel.LowercaseFilter, weasel.StopEn),
+		weasel.AddAnalyser("text.stemmed", weasel.Porter2Stemmer, weasel.LowercaseFilter, weasel.StopEn))
 
-	wi := weasel.NewInvertedIndex(args.Name, map[string]reflect.Kind{"text": reflect.String, "title": reflect.String})
+	client, err := elastic.NewClient(elastic.SetURL("http://localhost:9200"))
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	// TODO implement indexing.
+	// Scroll search.
+	svc := client.Scroll("ecir2018").
+		FetchSource(false).
+		Pretty(false).
+		Type("doc").
+		KeepAlive("1h").
+		SearchSource(
+		elastic.NewSearchSource().
+			From(0).
+			NoStoredFields().
+			FetchSource(true).
+			Size(10000).
+			TrackScores(false).
+			Query(elastic.NewMatchAllQuery()))
+
+	docs := 0
+
+	docsSince := 0
+	start := time.Now()
+
+	for docs < 28000 {
+		result, err := svc.Do(context.Background())
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		log.Printf("indexing next set of documents (%v) %v indexed so far\n", len(result.Hits.Hits), docs)
+
+		for _, hit := range result.Hits.Hits {
+
+			doc := weasel.NewDocument(hit.Id)
+			var source map[string]string
+			b, err := hit.Source.MarshalJSON()
+			if err != nil {
+				log.Fatal(err)
+			}
+			err = json.Unmarshal(b, &source)
+
+			if v, ok := source["title"]; ok {
+				doc.Set("title", v)
+			}
+
+			if v, ok := source["text"]; ok {
+				doc.Set("text", v)
+				doc.Set("text.stemmed", v)
+			}
+
+			err = wi.Index(doc)
+			if err != nil {
+				panic(err)
+			}
+			docs++
+			docsSince++
+
+			if docs > 28000 {
+				break
+			}
+
+			if time.Since(start) > 1*time.Second {
+				fmt.Printf("indexing at %v docs/second\n", docsSince)
+				start = time.Now()
+				docsSince = 0
+			}
+		}
+	}
+
+	err = svc.Clear(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("final size of the collection: %v\n", len(wi.DocumentMapping))
 
 	wi.Dump()
 }
--- cmd/wquery/main.go
+++ cmd/wquery/main.go
@@ -9,12 +9,13 @@ import (
 	"github.com/hscells/transmute/pipeline"
 	"github.com/hscells/weasel"
 	"log"
+	"fmt"
+	"time"
 )
 
 type args struct {
-	IndexFile  string `arg:"help:path to weasel index file.,required"`
-	IndexStore string `arg:"help:path to weasel index store.,required"`
-	Query      string `arg:"help:query to issue to weasel index."`
+	Index string `arg:"help:path to weasel index store.,required"`
+	Query string `arg:"help:query to issue to weasel index."`
 }
 
 func (args) Version() string {
@@ -29,11 +30,6 @@ func main() {
 
 	var args args
 	arg.MustParse(&args)
-
-	wi, err := weasel.LoadIndex(args.IndexFile, args.IndexStore)
-	if err != nil {
-		log.Fatal(err)
-	}
 
 	p := pipeline.NewPipeline(parser.NewCQRParser(),
 		backend.NewCQRBackend(),
@@ -54,13 +50,45 @@ func main() {
 		log.Fatal(err)
 	}
 
-	seen := make(map[uint64]weasel.LogicalTreeNode)
-	tree, _, err := wi.Execute(repr.(cqr.CommonQueryRepresentation), seen)
+	fmt.Println(repr)
+
+	log.Println("parsed query")
+
+	wi, err := weasel.LoadIndex(args.Index)
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 
-	log.Println("Documents")
-	log.Println(len(tree.Documents()))
+	log.Println("loaded index")
 
+	start := time.Now()
+	tree, _, err := wi.Execute(repr.(cqr.CommonQueryRepresentation))
+	if err != nil {
+		panic(err)
+	}
+
+	log.Println("executed query")
+
+	docs, err := tree.Documents()
+	if err != nil {
+		panic(err)
+	}
+
+	elapsed := time.Since(start)
+
+	log.Printf("retrieved %v documents in %v seconds", len(docs), elapsed.Seconds())
+
+	max := 10
+	for i, docID := range docs {
+		doc, err := wi.Get(docID)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("[%v] - %v\n", doc.ID, doc.Source["title"])
+		if i > max {
+			break
+		}
+	}
+
+	log.Println("done")
 }
